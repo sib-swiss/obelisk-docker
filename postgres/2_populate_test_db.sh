f82ed290178387246a676b31ced5dd3607f7d744
Iulian Dragan <iulian_dragan@yahoo.com>
2024-02-01 07:36:27 +0100

    docker-compose for postgres

--- postgres/2_populate_test_db.sh
+++ postgres/2_populate_test_db.sh
@@ -3,7 +3,7 @@ set -e
 export PGPASSWORD=opalpass
 
 # load the database
-psql -v ON_ERROR_STOP=1 --username postgres --dbname omop_test -f /scripts/OMOPCDM_postgresql_5.4_ddl.sql
-psql -v ON_ERROR_STOP=1 --username postgres --dbname omop_test -f /scripts/OMOPCDM_postgresql_5.4_indices.sql
-psql -v ON_ERROR_STOP=1 --username postgres --dbname omop_test -f /scripts/OMOPCDM_postgresql_5.4_primary_keys.sql
-#psql -v ON_ERROR_STOP=1 --username postgres --dbname omop_test -f /scripts/OMOPCDM_postgresql_5.4_constraints.sql
+psql -v ON_ERROR_STOP=1 --username postgres --dbname obelisk -f /scripts/OMOPCDM_postgresql_5.4_ddl.sql
+psql -v ON_ERROR_STOP=1 --username postgres --dbname obelisk -f /scripts/OMOPCDM_postgresql_5.4_indices.sql
+psql -v ON_ERROR_STOP=1 --username postgres --dbname obelisk -f /scripts/OMOPCDM_postgresql_5.4_primary_keys.sql
+#psql -v ON_ERROR_STOP=1 --username postgres --dbname obelisk -f /scripts/OMOPCDM_postgresql_5.4_constraints.sql
