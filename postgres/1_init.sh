3d4298e3302b021d868def363fa4d6a229e150ee
Iulian Dragan <iulian_dragan@yahoo.com>
2024-02-02 13:40:04 +0100

    ...

--- postgres/1_init.sh
+++ postgres/1_init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-export PGPASSWORD=opalpass
+export PGPASSWORD=$POSTGRES_PASSWORD
 psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
-    create user opalroot with encrypted password 'opalpass';
-    create user opal with encrypted password 'opalpass'; 
+    create user opalroot with encrypted password '$OPAL_PASSWORD';
+    create user opal with encrypted password '$OPAL_PASSWORD'; 
     
     create database opal_data;
     alter database opal_data owner to opalroot;
