9e583b0904d5538ae05639d77f800c88bc61e1a5
Iulian Dragan <iulian_dragan@yahoo.com>
2025-03-11 13:52:28 +0100

    ...

--- rock/postinstall.sh
+++ rock/postinstall.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-Rscript -e "install.packages(c('dsSwissKnife', 'resourcex', 'dsQueryLibraryServer'), repos = c($R_REPOS), upgrade = FALSE, lib = '/var/lib/rock/R/library')"
+for mylib in dsSwissKnife resourcex dsQueryLibraryServer dsMOprimal dsMOdual; do
+	[ -d "/var/lib/rock/R/library/$mylib" ] || Rscript -e "install.packages('$mylib', repos = c($R_REPOS), upgrade = FALSE, lib = '/var/lib/rock/R/library')"
+done
 chown -R rock /var/lib/rock/R/library
